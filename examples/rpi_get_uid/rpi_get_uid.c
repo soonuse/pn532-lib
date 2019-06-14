3913d26142ebf4d47429777f1d76fbe486f0ffc4
soonuse <116975428@qq.com>
2019-06-14 08:52:09 +0000

    Add an example

---
+++ examples/rpi_get_uid/rpi_get_uid.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+#include "pn532.h"
+#include "pn532_rpi.h"
+
+int main(int argc, char** argv) {
+    uint8_t buff[255];
+    uint8_t uid[MIFARE_UID_MAX_LENGTH];
+    int32_t uid_len = 0;
+    printf("Hello!\r\n");
+    PN532 pn532;
+    PN532_RPi_Init(&pn532);
+    if (PN532_GetFirmwareVersion(&pn532, buff) == PN532_STATUS_OK) {
+        printf("Found PN532 with firmware version: %d.%d\r\n", buff[1], buff[2]);
+    }
+    PN532_SamConfiguration(&pn532);
+    printf("Waiting for RFID/NFC card...\r\n");
+    while (1)
+    {
+        // Check if a card is available to read
+        uid_len = PN532_ReadPassiveTarget(&pn532, uid, PN532_MIFARE_ISO14443A, 1000);
+        if (uid_len == PN532_STATUS_ERROR) {
+            printf(".");
+            fflush(stdout);
+        } else {
+            printf("Found card with UID: ");
+            for (uint8_t i = 0; i < uid_len; i++) {
+                printf("%02x ", uid[i]);
+            }
+            printf("\r\n");
+            break;
+        }
+    }
+}
